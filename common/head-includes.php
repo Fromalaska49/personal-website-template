fe2321e4a89c64eab5811525e4e71299e2c1b190
Andrew Hutton <fromalaska49@gmail.com>
2017-01-12 15:06:46 -0600

    Added Local Copies of jQuery and Bootstrap for Offline Development

--- common/head-includes.php
+++ common/head-includes.php
@@ -2,13 +2,13 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <!-- CSS -->
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="css/master.css" />
 <link rel="stylesheet" type="text/css" href="css/font-awesome-4.7.0/css/font-awesome.min.css" />
 <link rel="icon" href="favicon.ico" type="image/x-icon" />
 <!-- JavaScript -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
 		$("#shell-link").on("mouseenter", function(){
